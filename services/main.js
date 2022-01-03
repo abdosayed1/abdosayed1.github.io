f83d9a5071ec5aa19adf0a1397cef2b8412fcedd
abdosayed1 <abdosbed6@gmail.com>
2022-01-03 19:00:04 +0200

    add refresh handler

--- services/main.js
+++ services/main.js
@@ -1,5 +1,5 @@
 setTimeout(redirectToHomePage, 3000);
 
 function redirectToHomePage(){
-    window.location.href = "https://abdosbedodev.000webhostapp.com/";
+    window.location.href = "https://sbedo.me/";
 }
